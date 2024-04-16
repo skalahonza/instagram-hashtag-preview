1a8e65a0f3c8b904f8a5eb312379939905834239
Jan Skála <j.skala@hotmail.com>
2024-04-16 13:54:40 +0200

    Add code files

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+﻿FROM --platform=linux/amd64 mcr.microsoft.com/dotnet/aspnet:8.0 AS base
+
+#####################
+#PUPPETEER RECIPE
+#####################
+RUN apt-get update && apt-get -f install && apt-get -y install wget gnupg2 apt-utils
+RUN wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list
+RUN apt-get update \
+&& apt-get install -y google-chrome-stable --no-install-recommends --allow-downgrades fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf
+######################
+#END PUPPETEER RECIPE
+######################
+ENV PUPPETEER_EXECUTABLE_PATH "/usr/bin/google-chrome-stable"
+
+USER $APP_UID
+WORKDIR /app
+EXPOSE 8080
+EXPOSE 8081
+
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+ARG BUILD_CONFIGURATION=Release
+WORKDIR /src
+COPY ["Instagram/Instagram.csproj", "Instagram/"]
+RUN dotnet restore "Instagram/Instagram.csproj"
+COPY . .
+WORKDIR "/src/Instagram"
+RUN dotnet build "Instagram.csproj" -c $BUILD_CONFIGURATION -o /app/build
+
+FROM build AS publish
+ARG BUILD_CONFIGURATION=Release
+RUN dotnet publish "Instagram.csproj" -c $BUILD_CONFIGURATION -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Instagram.dll"]
---
+++ Program.cs
@@ -0,0 +1,53 @@
+using PuppeteerSharp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(x =>
+    {
+        x.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        x.RoutePrefix = "";
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/photos/{hashtag}", async (string hashtag) =>
+    {
+        var urls = await GetImageUrls(hashtag).ToListAsync();
+        return urls;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+app.Run();
+return;
+
+async IAsyncEnumerable<string> GetImageUrls(string hashtag)
+{
+    await using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+    await using var page = await browser.NewPageAsync();
+    await page.GoToAsync($"https://www.instagram.com/explore/tags/{hashtag}/", WaitUntilNavigation.Networkidle0);
+    await page.WaitForTimeoutAsync(5000);
+
+    var images = await page.XPathAsync("//img");
+    foreach (var image in images)
+    {
+        var src = await image.EvaluateFunctionAsync<string>("e => e.src");
+        if (!src.Contains("http"))
+        {
+            continue;
+        }
+        yield return src;
+    }
+}
